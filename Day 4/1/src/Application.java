99b4bc113e30ce2c7f69a858f1a41aa6a694dd63
Iulian Preda <iulian.octavian.preda@gmail.com>
2018-12-04 14:47:05 +0200

    Solution for Day's 4 Problem 2 - version 2

--- Day 4/1/src/Application.java
+++ Day 4/1/src/Application.java
@@ -1,51 +1,15 @@
-
+import java.lang.reflect.Array;
+import java.util.stream.*;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.util.*;
 
 public class Application {
 
-    static int mostFrequent(ArrayList<Integer> arr)
-    {
-
-        // Sort the array
-        Collections.sort(arr);
-
-        // find the max frequency using linear
-        // traversal
-        int max_count = 1, res = arr.get(0);
-        int curr_count = 1;
-
-        for (int i = 1; i < arr.size(); i++)
-        {
-            if (arr.get(i) == arr.get(i - 1))
-                curr_count++;
-            else
-            {
-                if (curr_count > max_count)
-                {
-                    max_count = curr_count;
-                    res = arr.get(i - 1);
-                }
-                curr_count = 1;
-            }
-        }
-
-        // If last element is most frequent
-        if (curr_count > max_count)
-        {
-            max_count = curr_count;
-            res = arr.get(arr.size() - 1);
-        }
-
-        return res;
-    }
-
-
-    public static void main(String[] args) {
+     public static void main(String[] args) {
 
         ArrayList<String> entries = new ArrayList<>();
-        HashMap<Integer,ArrayList<Integer>> guards = new HashMap<>();
+        HashMap<Integer,int[]> guards = new HashMap<>();
 
         File file = new File("input.txt");
         Scanner sc = null;
@@ -66,21 +30,22 @@ public class Application {
         int awake = 0;
         int[] maxSleep =new int[2];
         int currentSleep = 0;
-        ArrayList<Integer> tempArr = new ArrayList<>();
+        int[] frequencyArray = new int[60];
 
         for (String entry: entries) {
             if(entry.contains("Guard")){
                 currentSleep = 0;
                 asleep = 0;
                 awake = 0;
-                tempArr = new ArrayList<>();
+                frequencyArray = new int[60];
                 String tempString = entry;
                 id = Integer.parseInt(tempString.substring(18).replaceAll("[^0-9]", ""));
                 if(guards.containsKey(id)){
-                    tempArr = guards.get(id);
-                    currentSleep = tempArr.size();
+                    frequencyArray = guards.get(id);
+                    currentSleep = IntStream.of(frequencyArray).sum();
+                }else{
+                    guards.put(id, frequencyArray);
                 }
-                guards.put(id, tempArr);
 
             }else{
                 if(entry.contains("falls")){
@@ -94,9 +59,9 @@ public class Application {
                         maxSleep[0] = id;
                     }
                     for(int i = asleep ; i < awake ; i++){
-                        tempArr.add(i);
+                        frequencyArray[i]++;
                     }
-                    guards.put(id, tempArr);
+                    guards.put(id, frequencyArray);
                 }
             }
             //System.out.println(entry);
@@ -106,8 +71,15 @@ public class Application {
             System.out.println(i);
         }
 
-        ArrayList<Integer> selectedGuard = guards.get(maxSleep[0]);
-        int frequentMinute = mostFrequent(selectedGuard);
+        frequencyArray = guards.get(maxSleep[0]);
+        int frequentMinute = 0;
+        int maxFrequency = 0;
+        for(int i = 0; i< 60; i++){
+            if(frequencyArray[i] > maxFrequency){
+                frequentMinute = i;
+                maxFrequency = frequencyArray[i];
+            }
+        }
         System.out.println("Id of the selected guard is: " + maxSleep[0] + " he has a maximum of " + maxSleep[1] + " minutes of sleep");
         System.out.println("The most frequent minute is " + frequentMinute);
         System.out.println("Result Id * frequentMinute: " + (maxSleep[0] * frequentMinute) );
